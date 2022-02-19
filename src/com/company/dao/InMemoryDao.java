770f79e9a7082ac0a212b1ad27235516cc6bfd9a
ahmetdellal <ahmet.dellal@testinium>
2022-02-19 17:01:10 +0300

    19 subat ddrsleri icin ornekler

--- src/com/company/dao/InMemoryDao.java
+++ src/com/company/dao/InMemoryDao.java
@@ -60,7 +60,7 @@ public class InMemoryDao {
                 .collect(Collectors.toList());
     }
 
-  //seamlat
+  //seamlar conflict varrr
 
     public List<Student> getNameIsZeynep(){
         return studentMap.values().stream()
